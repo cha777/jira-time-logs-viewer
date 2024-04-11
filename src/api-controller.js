7f3a548ee8c6c56641778661e5905a9267da4610
m_dasun <m.dasun@directfn.com>
2024-04-11 23:20:06 +0530

    searchIssues pagination

--- src/api-controller.js
+++ src/api-controller.js
@@ -18,15 +18,35 @@ function _getDefaultHeaders() {
 }
 
 exports.searchIssues = async function (jql) {
-  const res = await fetch(
-    defaultURL + `/rest/api/3/search?maxResults=${process.env.JIRA_MAX_SEARCH_RESULTS}&jql=${jql}`,
-    {
-      method: 'GET',
-      headers: _getDefaultHeaders(),
-      agent: httpsAgent,
+  let formattedRes;
+  let startAt = 0;
+  let pageCount = 1;
+  let issueList = [];
+
+  while (true) {
+    if (pageCount === 0) break;
+
+    let response  = await fetch(
+      defaultURL + `/rest/api/3/search?maxResults=${process.env.JIRA_MAX_SEARCH_RESULTS}&startAt=${startAt}&jql=${jql}`,
+      {
+        method: 'GET',
+        headers: _getDefaultHeaders(),
+        agent: httpsAgent,
+      }
+    );
+
+    formattedRes = await response.json();
+    issueList = issueList.concat(formattedRes.issues);
+
+    if (startAt === 0) {
+      pageCount = Math.ceil(formattedRes.total / formattedRes.maxResults);
     }
-  );
-  return await res.json();
+
+    startAt = startAt + formattedRes.maxResults;
+    pageCount = pageCount - 1
+  }
+
+  return issueList;
 };
 
 exports.getIssueWorklogs = async function (issueId, startedBefore, startedAfter) {
